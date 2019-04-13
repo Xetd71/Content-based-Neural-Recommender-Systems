bdea6f868fe60c14d0764fea083166b0b70dacef
Xetd71 <ischuev@edu.hse.ru>
2019-04-14 00:00:06 +0300

    ndcg@20 for testing models

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,9 +1,14 @@
 import os
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 
-def dcg_at_k(r, k, method=0):
+HOME_DIR = os.environ['HOME_DIR']
+DATA_DIR = f'{HOME_DIR}data/zen/'
+
+
+def dcg_at_k(r, k=20, method=0):
     """
     Args:
         r: Relevance scores (list or numpy) in rank order
@@ -25,7 +30,7 @@ def dcg_at_k(r, k, method=0):
     return 0.
 
 
-def ndcg_at_k(r, k, method=0):
+def ndcg_at_k(r, k=20, method=0):
     """
     Args:
         r: Relevance scores (list or numpy) in rank order
@@ -42,25 +47,44 @@ def ndcg_at_k(r, k, method=0):
     return dcg_at_k(r, k, method) / dcg_max
 
 
-HOME_DIR = os.environ['HOME_DIR']
-DATA_DIR = f'{HOME_DIR}data/zen/'
-perfect_ranking = pd.read_csv(f'{DATA_DIR}/perfect_ranking.csv')
-user_ids = set(perfect_ranking['userId'])
+class test:
+    solution = pd.read_csv(f'{DATA_DIR}/solution.csv')
+    ratings = {}
+    user_items = {}
+    user_ids = set(solution['userId'])
+    for uid in tqdm(user_ids):
+        uid_items = solution[solution['userId'] == uid]
+        user_items[uid] = uid_items['itemId'].tolist()
+        ratings[uid] = dict(zip(uid_items['itemId'], uid_items['rating']))
 
+    @staticmethod
+    def user_ndcg(user_id, ordered_items, k=20, method=1):
+        true_ratings = test.ratings[user_id]
+        return ndcg_at_k(list(map(lambda x: true_ratings[x], ordered_items)), k, method)
 
-def zen_ndcg_at_k(user_id, user_items, k=20, method=0):
-    if user_id in user_ids:
-        item_rel = dict(zip(user_items, np.arange(user_items.shape[0] - 1, -1, -1)))
-        perfect_order = perfect_ranking[perfect_ranking['userId'] == user_id]['itemId'].values
-        return ndcg_at_k(list(map(lambda x: item_rel[x], perfect_order)), 20, method=1)
-    return None
+    @staticmethod
+    def ndcg(predict, k=20, method=1):
+        ndcg = 0
+        for uid, items in tqdm(test.user_items.items()):
+            ndcg += test.user_ndcg(
+                uid,
+                np.array(sorted(
+                    zip(predict(uid, items), items),
+                    key=lambda x: x[0],
+                    reverse=True))[:, 1],
+                k, method
+            )
+        return ndcg / len(test.user_items)
 
 
 if __name__ == "__main__":
-    print(ndcg_at_k([3, 2, 3, 0, 0, 1, 2, 2, 3, 0], 1, method=1))
-    # print(ndcg_at_k([0], 5, method=1))
-    # print(ndcg_at_k([1], 5, method=1))
-    # print(ndcg_at_k([1, 0], 5, method=1))
-    # print(ndcg_at_k([0, 1], 5, method=1))
-    # print(ndcg_at_k([0, 1, 1], 5, method=1))
-    # print(ndcg_at_k([0, 1, 1, 1], 5, method=1))
+    click_rate_submission = pd.read_csv(f'{DATA_DIR}/click_rate_submission.csv')
+    
+    
+    def predict(uid, items):
+        predicted_order = click_rate_submission[click_rate_submission['userId'] == uid]['itemId'].values
+        predicted_order = dict(zip(predicted_order, np.arange(predicted_order.shape[0], 0, -1)))
+        return list(map(lambda x: predicted_order[x], items))
+    
+    
+    test.ndcg(predict)
